2ee47dcdddd24c3959a2c2039da8faacd446380c
almoval <romina.agripita1@gmail.com>
2022-06-26 22:52:06 -0300

    ultimoooooooo

---
+++ final/control final.js
@@ -0,0 +1,39 @@
+// console.log("funciona")
+// function calcular() {
+//     var a =document.getElementById("CantPlacas");
+//     var b =document.getElementById("med43x24");
+//     var c =document.getElementById("med24x24");
+//     var d =document.getElementById("med13x18");
+//     var e =document.getElementById("Final del Dia");
+    
+//     // const resultadoSuma = sumar(b,c,d);
+    
+//     const resultadoResta =restar(a,b,c,d);
+//     e.setAttribute("value",resultadoResta)
+//     // console.log(resultadoSuma);
+//     console.log(resultadoResta);    
+// }
+
+// // function sumar ( b , c , d) {
+// //     return b + c + d;
+// // }
+// function restar(a,b,c,d) {
+//     return e= a - b -c -d;
+// }
+// var a=parseInt(document.getElementById("CantPlacas").value);
+// var b=parseInt(document.getElementById("med43x24").value);
+// var c=parseInt(document.getElementById("med24x24").value);
+// var d=parseInt(document.getElementById("med13x18").value);
+// var restar document.getElementById("Final del Dia")=a-b-c-d;
+// document.form.resultado.value=resta;
+function restar(){
+    var a=parseInt(document.getElementById("CantPlacas").value);
+    var b=parseInt(document.getElementById("med43x24").value);
+    var c=parseInt(document.getElementById("med24x24").value);
+    var d=parseInt(document.getElementById("med13x18").value);
+    var resta=a-b-c-d;
+   document.form.button.value=resta;
+}
+    // <button type="button" class="botonSubmit" onClick="calcular()">calcular</button>
+    
+    
